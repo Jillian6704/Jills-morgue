de08b455591920f4d068d6befd7ab1e466cb308b
Jillian6704 <jacksonjk3@vcu.edu>
2023-07-29 15:06:37 -0400

    final edit

---
+++ jquery/scripts.js
@@ -0,0 +1,27 @@
+$(".dark-btn").click(
+    function(){
+     $(".box").toggleClass("dark");
+     $("body").toggleClass("dark");
+  });
+
+ $(".glow-btn").click(
+function(){
+    $(".box").toggleClass("glow");
+}
+ );
+
+ $(".spin-btn").click(
+    function(){
+        $(".box").toggleClass("spin");
+    }
+     );
+
+     $(".reveal-btn").click(
+        function(){
+            $(".face").css("opacity","1");
+            $(".reveal-btn").hide();
+        }
+         );
+
+            $( ".draggable" ).draggable();
+      
